faa75d39e360473db7434b9a2787b20b4ca55f16
HeroCub <131084125+HeroCub@users.noreply.github.com>
2026-09-09 15:34:47 -0300

    Clase Reptil.
    Creacion de la clase Reptil
    Se le agregaron dos atributos y ciertos metodos
    - esVenenoso
    - tienePatas
    - todos los metodos de la clase Animal

---
+++ tp_grupal/src/SistemaGestionZoologico/Reptil.java
@@ -0,0 +1,57 @@
+package SistemaGestionZoologico;
+
+public class Reptil extends Animal{
+
+    private boolean esVenenoso;
+    private boolean tienePatas;
+
+    public Reptil(String nombre, String especie, int hambre, int salud, int higiene, boolean esVenenoso, boolean tienePatas)
+    {
+        super(nombre, especie, hambre, salud, higiene);
+        this.esVenenoso = esVenenoso;
+        this.tienePatas = tienePatas;
+    }
+
+    public String comer()
+    {
+        return "Ejecucion de alimentarse";
+    }
+
+    public String enfermarse()
+    {
+        return "Ejecucion de enfermarse";
+    }
+
+    public String ensuciarse()
+    {
+        return "Ejecucion de ensuciarse";
+    }
+
+    public String toString()
+    {
+        String venenoso;
+        String tiene_patas;
+
+        if(this.esVenenoso == true)
+        {
+            venenoso = "Si";
+        }
+        else
+        {
+            venenoso = "No";
+        }
+
+        if(this.tienePatas == true)
+        {
+            tiene_patas = "Si";
+        }
+        else
+        {
+            tiene_patas = "No";
+        }
+
+        return "Animal [Nombre: "+super.getNombre()+", Especie: "+super.getEspecie()+", Hambre: "+super.getHambre()+
+                ", Salud: "+super.getSalud()+", Higiene: "+super.getHigiene()+", es venenoso: "+venenoso
+                +", tiene patas: "+tiene_patas+"]";
+    }
+}
