3f1331d263e5856f59cb6e61f9126a755de37a9d
HeroCub <131084125+HeroCub@users.noreply.github.com>
2026-09-09 22:35:39 -0300

    Creacion de la Clase Persona y correcion en la Clase Equipo.
    Creacion de tres atributos para la clase Persona:
    - Nombre
    - fecha_nacimiento
    - dni
    
    Se agrego ciertos metodos importantes en la clase Persona:
    - Todos los getters de sus atributos
    - metodo equals y hashcode
    - metodo toString
    
    Cambios en la clase Equipo:
    - El atributo entrenador dejara de ser de tipo String y pasa a ser de tipo Persona

--- tp_grupal/src/ClubDeportivo/Equipo.java
+++ tp_grupal/src/ClubDeportivo/Equipo.java
@@ -4,12 +4,11 @@ public class Equipo {
 
     private String nombre;
     private Deporte deporte;
-    private String entrenador;
+    private Persona entrenador;
     private Jugador jugador;
 
-    public Equipo(String nombre_equipo, String entrenador) {
+    public Equipo(String nombre_equipo) {
         this.nombre = nombre_equipo;
-        this.entrenador = entrenador;
     }
 
     public void agregarDeporte(String nombre, String descripcion, boolean pelota, double dimensionesCancha)
@@ -22,4 +21,24 @@ public class Equipo {
         this.jugador = new Jugador(this.deporte, categoria, nroCamiseta);
     }
 
+    public void agregarEntrenador(String nombre, String fecha_nacimiento, String dni)
+    {
+        this.entrenador = new Persona(nombre, fecha_nacimiento, dni);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Deporte getDeporte() {
+        return deporte;
+    }
+
+    public Persona getEntrenador() {
+        return entrenador;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
 }
---
+++ tp_grupal/src/ClubDeportivo/Persona.java
@@ -0,0 +1,64 @@
+package ClubDeportivo;
+
+import java.util.Objects;
+
+public class Persona {
+
+    private String nombre;
+    private String fecha_nacimiento;
+    private String dni;
+
+    public Persona(String nombre, String fecha_nacimiento, String dni)
+    {
+        this.nombre = nombre;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public boolean equals(Object obj)
+    {
+        boolean rta = true;
+
+        if(obj == null)
+        {
+            rta = false;
+        }
+        else
+        {
+            if(obj instanceof Persona)
+            {
+                Persona p = (Persona) obj;
+                if(p.getDni() == this.dni)
+                {
+                    rta = true;
+                }
+            }
+        }
+
+        return rta;
+    }
+
+    public int hashcode()
+    {
+        return Objects.hash(this.getDni());
+    }
+
+    public String toString()
+    {
+        return "Persona [Nombre: "+this.nombre+", Fecha de Nacimiento: "+this.fecha_nacimiento
+                +", DNI: "+this.dni+"]";
+    }
+
+}
